2456ceb905c2568ee6dd6838a5f6b28ee26ff6c9
kron0zzz <juanriosr7526@gmail.com>
2026-05-11 16:09:17 -0500

    fase 3: api funcional (suppliers)

--- src/infrastructure/repositories/SupplierRepositoryPrisma.js
+++ src/infrastructure/repositories/SupplierRepositoryPrisma.js
@@ -2,22 +2,24 @@ import pool from "../../config/database.js";
 
 export default class SupplierRepository {
 
- /* async create(supplierData) {
+  async create(supplierData) {
 
-    const { name, phone, email } = supplierData;
+    const { document_type, document_number,supplier_status, supplier_name, supplier_address, supplier_phone, supplier_email, supplier_state, supplier_city } = supplierData;
 
     const query = `
-      INSERT INTO suppliers (name, phone, email)
-      VALUES ($1, $2, $3)
+      INSERT INTO suppliers (document_type, document_number,supplier_status, supplier_name, supplier_address, supplier_phone, supplier_email, supplier_state, supplier_city)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
       RETURNING *
     `;
 
-    const values = [name, phone, email];
+    const values = [document_type, document_number,supplier_status, supplier_name, supplier_address, supplier_phone, supplier_email, supplier_state, supplier_city];
 
     const result = await pool.query(query, values);
 
     return result.rows[0];
-  }*/
+  }
+
+
   async findAll() {
 
     const result =
@@ -26,30 +28,40 @@ export default class SupplierRepository {
     return result.rows;
   }
 
-  /*async findById(id) {
+
+
+  async findById(id) {
 
     const result = await pool.query(
-      "SELECT * FROM suppliers WHERE id = $1",
+      "SELECT * FROM suppliers WHERE supplier_id = $1",
       [id]
     );
 
     return result.rows[0];
   }
 
+
+  
   async update(id, supplierData) {
 
-    const { name, phone, email } = supplierData;
+    const { document_type, document_number,supplier_status, supplier_name, supplier_address, supplier_phone, supplier_email, supplier_state, supplier_city } = supplierData;
 
     const query = `
       UPDATE suppliers
-      SET name = $1,
-          phone = $2,
-          email = $3
-      WHERE id = $4
+      SET document_type = $1, 
+      document_number = $2,
+      supplier_status = $3, 
+      supplier_name = $4, 
+      supplier_address = $5, 
+      supplier_phone = $6, 
+      supplier_email = $7, 
+      supplier_state = $8, 
+      supplier_city = $9
+      WHERE supplier_id = $10
       RETURNING *
     `;
 
-    const values = [name, phone, email, id];
+    const values = [ document_type, document_number,supplier_status, supplier_name, supplier_address, supplier_phone, supplier_email, supplier_state, supplier_city, id];
 
     const result =
       await pool.query(query, values);
@@ -57,13 +69,14 @@ export default class SupplierRepository {
     return result.rows[0];
   }
 
+  
   async delete(id) {
 
     const result = await pool.query(
-      "DELETE FROM suppliers WHERE id = $1 RETURNING *",
+      "DELETE FROM suppliers WHERE supplier_id = $1 RETURNING *",
       [id]
     );
 
     return result.rows[0];
-  }*/
+  }
 }
--- src/infrastructure/routes/supplierRoutes.js
+++ src/infrastructure/routes/supplierRoutes.js
@@ -3,10 +3,10 @@ import { createSupplier, getSuppliers, getSupplierById, updateSupplier, deleteSu
 
 const router = Router();
 
-router.post("/suppliers/", createSupplier);
+router.post("/", createSupplier);
 router.get("/", getSuppliers);
-router.get("/suppliers/:id", getSupplierById);
-router.put("/suppliers/:id", updateSupplier);
-router.delete("/suppliers/:id", deleteSupplier);
+router.get("/:id", getSupplierById);
+router.put("/:id", updateSupplier);
+router.delete("/:id", deleteSupplier);
 
 export default router;
