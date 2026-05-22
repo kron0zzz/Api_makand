340413a4e7e2f1cb53997ec0526f14b0bcd00afc
Sara-Ortiz <saracamilaoh@gmail.com>
2026-05-22 09:19:01 -0500

    fix: actualizar configuracion de rutas

--- src/interfaces/server.js
+++ src/interfaces/server.js
@@ -3,10 +3,12 @@ import cors from "cors"; // 1. IMPORTA CORS
 
 import supplierRoutes from "../infrastructure/routes/supplierRoutes.js";    //agregar rutas
 import vehicleRoutes from "../infrastructure/routes/vehicleRoutes.js";
-import customerRoutes from "../infrastructure/routes/CustomerRoutes.js";
+import customerRoutes from "../infrastructure/routes/customerRoutes.js";
 import positionRoutes from "../infrastructure/routes/positionRoutes.js"
 import machineryStatusRoutes from "../infrastructure/routes/machineryStatusRoutes.js";
-import machineryCategoryRoutes from "../infrastructure/routes/machineryCategoryRoutes.js"; // NUEVA IMPORTACIÓN
+import machineryCategoryRoutes from "../infrastructure/routes/machineryCategoryRoutes.js";
+import machineryRoutes from "../infrastructure/routes/machineryRoutes.js";
+
 const app = express();
 app.use(cors()); //2. ACTIVA CORS antes de las rutas
 app.use(express.json());
@@ -23,6 +25,7 @@ app.use("/api/vehicles", vehicleRoutes);
 app.use("/api/positions", positionRoutes);
 app.use("/api/machine-statuses", machineryStatusRoutes);
 app.use("/api/machine-categories", machineryCategoryRoutes); 
+app.use("/api/machines", machineryRoutes);
 export default app;
 
 
