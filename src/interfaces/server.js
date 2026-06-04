3cce21cd1db96c59207259dec9fbdf081f95b977
Sara-Ortiz <saracamilaoh@gmail.com>
2026-06-04 06:45:55 -0500

    feat: factura compra

--- src/interfaces/server.js
+++ src/interfaces/server.js
@@ -1,15 +1,9 @@
 import express from "express";
-<<<<<<< Updated upstream
-import cors from "cors";
-=======
 import cors from "cors"; 
->>>>>>> Stashed changes
 
-import supplierRoutes from "../infrastructure/routes/supplierRoutes.js";
+import supplierRoutes from "../infrastructure/routes/supplierRoutes.js";    //agregar rutas
 import vehicleRoutes from "../infrastructure/routes/vehicleRoutes.js";
-import customerRoutes from "../infrastructure/routes/CustomerRoutes.js";
-import chargeTypeRoutes from "../infrastructure/routes/chargeTypeRoutes.js";
-import projectRoutes from "../infrastructure/routes/projectRoutes.js";
+import customerRoutes from "../infrastructure/routes/customerRoutes.js";
 import positionRoutes from "../infrastructure/routes/positionRoutes.js"
 import machineryStatusRoutes from "../infrastructure/routes/machineryStatusRoutes.js";
 import machineryCategoryRoutes from "../infrastructure/routes/machineryCategoryRoutes.js";
@@ -17,14 +11,11 @@ import machineryRoutes from "../infrastructure/routes/machineryRoutes.js";
 import userRoutes from "../infrastructure/routes/userRoutes.js"
 import authRoutes from "../infrastructure/routes/authRoutes.js"
 import employeeRoutes from "../infrastructure/routes/employeeRoutes.js";
-<<<<<<< Updated upstream
-=======
 import purchaseInvoiceRoutes from "../infrastructure/routes/purchaseInvoiceRoutes.js";
 
 
->>>>>>> Stashed changes
 const app = express();
-app.use(cors());
+app.use(cors()); //2. ACTIVA CORS antes de las rutas
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -33,11 +24,9 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use("/api/suppliers", supplierRoutes);  //usar rutasapp.use("/api/customers", customerRoutes);
+app.use("/api/suppliers", supplierRoutes);  //usar rutas
 app.use("/api/customers", customerRoutes);
 app.use("/api/vehicles", vehicleRoutes);
-app.use("/api/charge-types", chargeTypeRoutes);
-app.use("/api/projects", projectRoutes);
 app.use("/api/positions", positionRoutes);
 app.use("/api/machine-statuses", machineryStatusRoutes);
 app.use("/api/machine-categories", machineryCategoryRoutes); 
