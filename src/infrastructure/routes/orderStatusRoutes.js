ff42de7b35a18d20b4c99ac4ef72d8a32477137d
Sara-Ortiz <saracamilaoh@gmail.com>
2026-07-01 09:04:58 -0500

    fix: actualizacion de ruta

--- src/infrastructure/routes/orderStatusRoutes.js
+++ src/infrastructure/routes/orderStatusRoutes.js
@@ -14,7 +14,7 @@ const router = Router();
 
 router.get("/table", authMiddleware, authorize('Listar Estado de Orden'), getOrderStatussTable);
 router.post("/", authMiddleware, authorize('Crear Estado de Orden'), createOrderStatus);
-router.get("/", authMiddleware, authorize('Listar Estados de Orden'), getOrderStatuss);
+router.get("/", authMiddleware, authorize('Listar Estado de Orden'), getOrderStatuss);
 router.get("/:id", authMiddleware, authorize('Ver Detalle de Estado de Orden'), getOrderStatusById);
 router.put("/:id", authMiddleware, authorize('Editar Estado de Orden'), updateOrderStatus);
 router.delete("/:id", authMiddleware, authorize('Eliminar Estado de Orden'), deleteOrderStatus);
