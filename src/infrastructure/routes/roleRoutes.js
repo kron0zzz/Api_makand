62f7bc67bc273a3fc3f998f222d5953d25736f69
Sara-Ortiz <saracamilaoh@gmail.com>
2026-07-16 09:39:27 -0500

    avance

--- src/infrastructure/routes/roleRoutes.js
+++ src/infrastructure/routes/roleRoutes.js
@@ -1,3 +1,37 @@
+// import { Router } from "express";
+// import authMiddleware from "../../middlewares/authMiddleware.js";
+// import authorize from "../../middlewares/authorize.js";
+// import { 
+//   createRole, 
+//   getRoles, 
+//   getRoleById, 
+//   updateRole, 
+//   deleteRole, 
+//   getRolesTable, 
+//   getPermissions, 
+//   getRoleByPermissions 
+// } from "../controllers/roleController.js"; 
+
+// const router = Router();
+
+// // Rutas fijas
+// router.get("/table", authMiddleware, authorize('Listar Rol'), getRolesTable);
+// router.get("/permissions/list", authMiddleware, authorize('Listar Rol'), getPermissions);
+
+// // Rutas dinámicas (Restauramos el authorize)
+// router.get("/:id/permissions", authMiddleware, authorize('Ver Detalle de Rol'), getRoleByPermissions);
+// router.get("/:id", authMiddleware, authorize('Ver Detalle de Rol'), getRoleById);
+
+// // Acciones generales
+// router.post("/", authMiddleware, authorize('Crear Rol'), createRole);
+// router.get("/", authMiddleware, authorize('Listar Rol'), getRoles);
+// router.put("/:id", authMiddleware, authorize('Editar Rol'), updateRole);
+// router.delete("/:id", authMiddleware, authorize('Eliminar Rol'), deleteRole);
+
+// export default router;
+
+
+
 import { Router } from "express";
 import authMiddleware from "../../middlewares/authMiddleware.js";
 import authorize from "../../middlewares/authorize.js";
@@ -9,7 +43,8 @@ import {
   deleteRole, 
   getRolesTable, 
   getPermissions, 
-  getRoleByPermissions 
+  getRoleByPermissions, 
+  updateRolePermissions
 } from "../controllers/roleController.js"; 
 
 const router = Router();
@@ -18,7 +53,8 @@ const router = Router();
 router.get("/table", authMiddleware, authorize('Listar Rol'), getRolesTable);
 router.get("/permissions/list", authMiddleware, authorize('Listar Rol'), getPermissions);
 
-// Rutas dinámicas (Restauramos el authorize)
+// Rutas dinámicas
+router.put("/:id/permissions", authMiddleware, authorize('Editar Rol'), updateRolePermissions);
 router.get("/:id/permissions", authMiddleware, authorize('Ver Detalle de Rol'), getRoleByPermissions);
 router.get("/:id", authMiddleware, authorize('Ver Detalle de Rol'), getRoleById);
 
