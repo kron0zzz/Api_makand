191fb017c594e6407c91bd0e95a358360a8a1f17
Sara-Ortiz <saracamilaoh@gmail.com>
2026-07-17 19:58:47 -0500

    funcionaaa

--- src/infrastructure/routes/roleRoutes.js
+++ src/infrastructure/routes/roleRoutes.js
@@ -1,37 +1,3 @@
-// import { Router } from "express";
-// import authMiddleware from "../../middlewares/authMiddleware.js";
-// import authorize from "../../middlewares/authorize.js";
-// import { 
-//   createRole, 
-//   getRoles, 
-//   getRoleById, 
-//   updateRole, 
-//   deleteRole, 
-//   getRolesTable, 
-//   getPermissions, 
-//   getRoleByPermissions 
-// } from "../controllers/roleController.js"; 
-
-// const router = Router();
-
-// // Rutas fijas
-// router.get("/table", authMiddleware, authorize('Listar Rol'), getRolesTable);
-// router.get("/permissions/list", authMiddleware, authorize('Listar Rol'), getPermissions);
-
-// // Rutas dinámicas (Restauramos el authorize)
-// router.get("/:id/permissions", authMiddleware, authorize('Ver Detalle de Rol'), getRoleByPermissions);
-// router.get("/:id", authMiddleware, authorize('Ver Detalle de Rol'), getRoleById);
-
-// // Acciones generales
-// router.post("/", authMiddleware, authorize('Crear Rol'), createRole);
-// router.get("/", authMiddleware, authorize('Listar Rol'), getRoles);
-// router.put("/:id", authMiddleware, authorize('Editar Rol'), updateRole);
-// router.delete("/:id", authMiddleware, authorize('Eliminar Rol'), deleteRole);
-
-// export default router;
-
-
-
 import { Router } from "express";
 import authMiddleware from "../../middlewares/authMiddleware.js";
 import authorize from "../../middlewares/authorize.js";
