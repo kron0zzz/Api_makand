dabdac1f2fdd4a50df7d782f97da3fd622102189
Sara-Ortiz <saracamilaoh@gmail.com>
2026-07-23 07:02:11 -0500

    tabla actualizada, switch

--- src/application/use-cases/users/UpdateUser.js
+++ src/application/use-cases/users/UpdateUser.js
@@ -1,12 +1,38 @@
+// export default class UpdateUser {
+//   constructor(userRepository) {
+//     this.userRepository = userRepository;
+//   }
+
+//   async execute(id, userData) {
+//     return await this.userRepository.update(
+//       id,
+//       userData
+//     );
+//   }
+// }
+
 export default class UpdateUser {
   constructor(userRepository) {
     this.userRepository = userRepository;
   }
 
   async execute(id, userData) {
-    return await this.userRepository.update(
-      id,
-      userData
-    );
+    // Buscamos el usuario actual en la base de datos
+    const existingUser = await this.userRepository.findById(id);
+    if (!existingUser) {
+      throw new Error("Usuario no encontrado");
+    }
+
+    // Si no se envía una contraseña nueva, mantenemos la que ya tenía guardada
+    const passwordToSave = (userData.user_password && userData.user_password.trim() !== "")
+      ? userData.user_password 
+      : existingUser.user_password;
+
+    const dataToUpdate = {
+      ...userData,
+      user_password: passwordToSave
+    };
+
+    return await this.userRepository.update(id, dataToUpdate);
   }
 }
