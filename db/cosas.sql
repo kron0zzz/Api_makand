83d4948eae4360fc58baa64f7d69f1d5e6a7c261
kon0zzz <juanriosr7526@gmail.com>
2026-08-18 15:41:22 +0000

    mejorar el script de cositas

--- db/cosas.sql
+++ db/cosas.sql
@@ -10,7 +10,11 @@ insert into machinery_stock (machinery_id, status_id, serial_number, is_owned, s
 insert into machinery_stock (machinery_id, status_id, serial_number, is_owned, stock_quantity) values (2, 1, 'TAL-003', true, 1);
 
 
+INSERT INTO suppliers (document_type, document_number, supplier_status, supplier_name, supplier_phone, supplier_state, supplier_city) VALUES ('CC', '10234454', true, 'Ferretería Vasquez', '3114553423', 'Antioquia', 'Caldas');
+
 INSERT INTO purchase_invoices (supplier_id, purchase_date, total_amount) VALUES (1, '2026-08-07', 5000000);
 
 INSERT INTO purchase_invoice_details (invoice_id, machinery_id, quantity, unit_cost, subtotal) VALUES (1, 2, 200, 4000, 6000);
 INSERT INTO purchase_invoice_details (invoice_id, machinery_id, quantity, unit_cost, subtotal) VALUES (1, 1, 1, 4000000, 4000000);
+
+INSERT INTO customers (organization_type, customer_document_type, customer_document_number, customer_status, customer_name, legal_representative, customer_phone) VALUES ('Jurídica', 'NIT', '4556654', true, 'Argos', 'Julian Mazo', '3024453232');
